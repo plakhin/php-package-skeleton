afb05b6eadd10dcc4383d0f6ba3c3825842ccb70
Stanislav Plakhin <plakhin@gmail.com>
2025-04-19 18:01:27 +0500

    add Rector cache caching to `rector` GH Action

--- rector.php
+++ rector.php
@@ -2,10 +2,15 @@
 
 declare(strict_types=1);
 
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
 return RectorConfig::configure()
+    ->withCache(
+        cacheClass: FileCacheStorage::class,
+        cacheDirectory: 'tmp/rector'
+    )
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
